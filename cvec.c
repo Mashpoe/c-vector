edcada1b49754b23177c1dcdcab585112bd5d547
Mashpoe <mashpoesemail@gmail.com>
2019-06-08 13:30:14 -0600

    lowercase

--- cvec.c
+++ cvec.c
@@ -1,9 +1,7 @@
 //
 //  cvec.c
-//  CHeckScript
 //
 //  Created by Mashpoe on 2/26/19.
-//  Copyright © 2019 Mashpoe. All rights reserved.
 //
 
 #include "cvec.h"
@@ -18,17 +16,17 @@ struct vector_data {
 	char buff[]; // use char to store bytes of an unknown type
 };
 
-vector_data* VECTOR_ALLOC(size_t alloc, size_t size) {
+vector_data* vector_alloc(size_t alloc, size_t size) {
 	vector_data* v_data = malloc(sizeof(vector_data) + alloc * size);
 	v_data->alloc = alloc;
 	return v_data;
 }
 
-vector_data* VECTOR_DATA(vector v) {
+vector_data* vector_get_data(vector v) {
 	return &((vector_data*)v)[-1];
 }
 
-vector VECTOR_CREATE(size_t size) {
+vector vector_create(size_t size) {
 	vector_data* v = malloc(sizeof(vector_data));
 	v->size = size;
 	v->alloc = 0;
@@ -37,48 +35,48 @@ vector VECTOR_CREATE(size_t size) {
 	return &v->buff;
 }
 
-void VECTOR_FREE(vector v) {
-	free(VECTOR_DATA(v));
+void vector_free(vector v) {
+	free(vector_get_data(v));
 }
 
-size_t VECTOR_SIZE(vector v) {
-	return VECTOR_DATA(v)->length;
+size_t vector_size(vector v) {
+	return vector_get_data(v)->length;
 }
 
-size_t VECTOR_GET_ALLOC(vector v) {
-	return VECTOR_DATA(v)->alloc;
+size_t vector_get_alloc(vector v) {
+	return vector_get_data(v)->alloc;
 }
 
-vector_data* VECTOR_REALLOC(vector_data* v_data) {
+vector_data* vector_realloc(vector_data* v_data) {
 	size_t new_alloc = (v_data->alloc == 0) ? 1 : v_data->alloc * 2;
 	vector_data* new_v_data = realloc(v_data, sizeof(vector_data) + new_alloc * v_data->size);
 	new_v_data->alloc = new_alloc;
 	return new_v_data;
 }
 
-bool VECTOR_HAS_SPACE(vector_data* v_data) {
+bool vector_has_space(vector_data* v_data) {
 	return v_data->alloc - v_data->length > 0;
 }
 
-void* VECTOR_ADD(vector* v) {
-	vector_data* v_data = VECTOR_DATA(*v);
+void* vector_add(vector* v) {
+	vector_data* v_data = vector_get_data(*v);
 	
-	if (!VECTOR_HAS_SPACE(v_data)) {
-		v_data = VECTOR_REALLOC(v_data);
+	if (!vector_has_space(v_data)) {
+		v_data = vector_realloc(v_data);
 		*v = v_data->buff;
 	}
 	
 	return (void*)&v_data->buff[v_data->size * v_data->length++];
 }
 
-void* VECTOR_INSERT(vector* v, size_t pos) {
-	vector_data* v_data = VECTOR_DATA(*v);
+void* vector_insert(vector* v, size_t pos) {
+	vector_data* v_data = vector_get_data(*v);
 	
 	size_t new_length = v_data->length + 1;
 	
 	// make sure there is enough room for the new element
-	if (!VECTOR_HAS_SPACE(v_data)) {
-		v_data = VECTOR_REALLOC(v_data);
+	if (!vector_has_space(v_data)) {
+		v_data = vector_realloc(v_data);
 	}
 	memmove(&v_data->buff[(pos+1) * v_data->size],
 			&v_data->buff[pos * v_data->size],
@@ -89,8 +87,8 @@ void* VECTOR_INSERT(vector* v, size_t pos) {
 	return &v_data->buff[pos * v_data->size];
 }
 
-void VECTOR_ERASE(vector* v, size_t pos, size_t len) {
-	vector_data* v_data = VECTOR_DATA(v);
+void vector_erase(vector* v, size_t pos, size_t len) {
+	vector_data* v_data = vector_get_data(v);
 	// anyone who puts in a bad index can face the consequences on their own
 	memmove(&v_data->buff[pos * v_data->size],
 			&v_data->buff[(pos+len) * v_data->size],
@@ -99,6 +97,6 @@ void VECTOR_ERASE(vector* v, size_t pos, size_t len) {
 	v_data->length -= len;
 }
 
-void VECTOR_REMOVE(vector* v, size_t pos) {
-	VECTOR_ERASE(v, pos, 1);
+void vector_remove(vector* v, size_t pos) {
+	vector_erase(v, pos, 1);
 }
